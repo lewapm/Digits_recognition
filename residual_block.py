1ac548ac99d2a94e005bb108b14c9de32474b7da
pawel <pawelmader11@gmail.com>
2020-11-27 17:59:44 +0100

    Add ResidualBlock implementation

--- residual_block.py
+++ residual_block.py
@@ -7,12 +7,15 @@ class ResidualBlock(nn.Module):
         super(ResidualBlock, self).__init__()
         self.conv2d1 = nn.Conv2d(channels, channels, kernel_size=kernel_size, stride=stride, padding=padding)
         self.conv2d2 = nn.Conv2d(channels, channels, kernel_size=kernel_size, stride=stride, padding=padding)
+        self.bn1 = nn.BatchNorm2d(channels)
+        self.bn2 = nn.BatchNorm2d(channels)
         self.activation_fn = activation_fn()
 
     def forward(self, x):
         residual = x
-        out = self.activation_fn(self.conv2d1(x))
-        out = self.activation_fn(self.conv2d2(out))
+
+        out = self.activation_fn(self.bn1(self.conv2d1(x)))
+        out = self.activation_fn(self.bn2(self.conv2d2(out)))
 
         out += residual
         return out
